22d471b58bc8725759e6facd4614459fd0a4af23
akelleci <a.kelleci@portbase.com>
2026-01-07 17:27:38 +0100

    spring boot 4

--- src/test/java/com/example/easysourcingexamplespring/EasysourcingExampleSpringApplicationTests.java
+++ src/test/java/com/example/easysourcingexamplespring/EasysourcingExampleSpringApplicationTests.java
@@ -1,8 +1,10 @@
 package com.example.easysourcingexamplespring;
 
 import com.example.easysourcingexamplespring.domain.CustomerCommand;
+import com.example.easysourcingexamplespring.domain.CustomerCommand.AddCredits;
 import com.example.easysourcingexamplespring.domain.CustomerCommand.CreateCustomer;
 import com.example.easysourcingexamplespring.domain.CustomerEvent;
+import com.example.easysourcingexamplespring.domain.CustomerEvent.CreditsAdded;
 import com.example.easysourcingexamplespring.domain.CustomerEvent.CustomerCreated;
 import com.example.easysourcingexamplespring.handlers.CustomerCommandHandler;
 import com.example.easysourcingexamplespring.handlers.CustomerEventSourcingHandler;
@@ -62,7 +64,7 @@ class EasysourcingExampleSpringApplicationTests {
 
 
 	@Test
-	void CustomerCreatedTest() {
+	void AddCreditsTest() {
 		Command command = Command.builder()
 				.payload(CreateCustomer.builder()
 						.id("cust-1")
@@ -74,12 +76,22 @@ class EasysourcingExampleSpringApplicationTests {
 		// publish command to topic with aggregateId as key!
 		commands.pipeInput(command.getAggregateId(), command);
 
+		 command = Command.builder()
+				.payload(AddCredits.builder()
+						.id("cust-1")
+						.build())
+				.build();
+
+		// publish command to topic with aggregateId as key!
+		commands.pipeInput(command.getAggregateId(), command);
+
+
 		// read events
 		List<Event> result = events.readValuesToList();
 
 		// assert
-		assertThat(result.size(), equalTo(1));
-		Event event = result.get(0);
-		assertThat(event.getPayload(), instanceOf(CustomerCreated.class));
+		assertThat(result.size(), equalTo(2));
+		assertThat(result.get(0).getPayload(), instanceOf(CustomerCreated.class));
+		assertThat(result.get(1).getPayload(), instanceOf(CreditsAdded.class));
 	}
 }
